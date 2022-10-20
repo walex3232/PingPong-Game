6b8582792021c3e3dce8f9669662a1c5b94c0b8e
Olawale Rasaki Olafisoye <104360945+walex3232@users.noreply.github.com>
2022-10-20 08:58:56 -0400

    Redone

--- pong.js
+++ pong.js
@@ -1,177 +1,71 @@
-const cvs = document.getElementById("pong");
-const ctx = cvs.getContext("2d");
-
-// creating user paddle
-
-const user = {
-    x : 0,
-    y : cvs.height/2 - 100/2,
-    width : 10,
-    height : 100,
-    color : "WHITE",
-    score : 0
-}
-
-// creating com paddle
-
-const com = {
-    x : cvs.width - 10,
-    y : cvs.height/2 - 100/2,
-    width : 10,
-    height : 100,
-    color : "WHITE",
-    score : 0
-}
-
-// creating ball 
-const ball = {
-    x : cvs.width/2,
-    y : cvs.height/2,
-    radius : 10,
-    speed : 5,
-    velocityX : 5,
-    velocityY : 5,
-    color : "WHITE"
-}
-
-// Creating draw rect function
-function drawRect(x,y,w,h,color){
-    ctx.fillStyle = color;
-    ctx.fillRect(x,y,w,h);
-}
-
-// creating net
-const net = {
-    x : cvs.width/2 - 1,
-    y : 0,
-    width : 2,
-    height : 10,
-    color : "WHITE"
-}
-
-// drawing net
-function drawNet(){
-    for(let i = 0; i <= cvs.height; i+=15){
-        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+function computerPlay(){
+    const random = (Math.floor(Math.random() * 3))
+    if(random === 0) {
+        return 'rock'
+    } else if(random === 1) {
+        return 'paper'
+    } else {
+        return 'scissors'
     }
 }
 
-// drawing Circle
-function drawCircle(x,y,r,color){
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(x,y,r,0,Math.PI*2,false);
-    ctx.closePath();
-    ctx.fill();
-}
+function playGround(playerSelection, computerSelection){
+    let winner = `You win! ${playerSelection} beats ${computerSelection}!`
+    let loser = `You lose! ${computerSelection} beats ${playerSelection}!`
+    let tie = 'It is a tie!'
 
-// drawing text
-
-function drawText(text,x,y,color){
-    ctx.fillStyle = color;
-    ctx.font = "45px fantasy";
-    ctx.fillText(text,x,y);
-}
-
-//rendering the game
-function render(){
-    // clear the canvas
-    drawRect(0, 0, cvs.width, cvs.height, "BLACK");
-
-    // drawing net
-    drawNet();
-
-    // drawing score
-    drawText(user.score,cvs.width/4,cvs.height/5,"WHITE");
-    drawText(com.score,3*cvs.width/4,cvs.height/5,"WHITE");
-
-    // drawing the user and com paddle
-    drawRect(user.x, user.y, user.width, user.height, user.color);
-    drawRect(com.x, com.y, com.width, com.height, com.color);
-
-    // drawing ball
-    drawCircle(ball.x, ball.y, ball.radius, ball.color);
-}
-
-// control the user paddle
-cvs.addEventListener("mousemove",movePaddle);
-
-function movePaddle(evt){
-    let rect = cvs.getBoundingClientRect();
-
-    user.y = evt.clientY - rect.top - user.height/2;
-}
-
-// collision detection
-function collision(b,p){
-    b.top = b.y - b.radius;
-    b.bottom = b.y + b.radius;
-    b.left = b.x - b.radius;
-    b.right = b.x + b.radius;
-
-    p.top = p.y;
-    p.bottom = p.y + p.height;
-    p.left = p.x;
-    p.right = p.x + p.width;
-
-    return b.right > p.left && b.bottom > p.top && b.left < p.right && b.top < p.bottom;
-}
-
-function resetBall(){
-    ball.x = cvs.width/2;
-    ball.y = cvs.height/2;
-
-    ball.speed = 5;
-    ball.velocityX = -ball.velocityX;
-}
-
-// updating pos, mov, score, and so on .....
-function update(){
-    ball.x += ball.velocityX;
-    ball.y += ball.velocityY;
-
-    // creating AI to control the com paddle
-    let computerLevel = 0.1;
-    com.y += (ball.y - (com.y + com.height/2)) * computerLevel;
-
-    if(ball.y + ball.radius > cvs.height || ball.y - ball.radius < 0){
-        ball.velocityY = -ball.velocityY;
+    if(playerSelection === computerSelection) {
+        return tie
     }
 
-    let player = (ball.x < cvs.width/2) ? user : com;
-
-    if(collision(ball,player)){
-        // where the ball will hit the player
-        let collidePoint = ball.y - (player.y + player.height/2);
-
-        collidePoint = collidePoint/(player.height/2);
-
-        let angleRad = collidePoint * Math.PI/4;
-
-        let direction = (ball.x < cvs.width/2) ? 1 : -1;
-
-        ball.velocityX = direction * ball.speed * Math.cos(angleRad);
-        ball.velocityY =             ball.speed * Math.sin(angleRad);
-
-        ball.speed += 0.5;
+    if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        return winner
+    } else if(playerSelection ==='scissors' && computerSelection === 'paper') {
+        return winner
+    } else if(playerSelection ==='paper' && computerSelection === 'rock') {
+        return winner
+    } else {
+        return loser
     }
+    
+}
 
-    if(ball.x - ball.radius < 0){
-
-        com.score++;
-        resetBall();
-    }else if(ball.x + ball.radius > cvs.width){
-        user.score++;
-        resetBall();
+function pong () {
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let i=0; i < 5; i++){
+        let playerSelection = prompt("Type 'rock' 'scissors' or 'paper'")
+        const playerSelected = playerSelection.toLowerCase();
+        const computerSelected = computerPlay()
+        if (playerSelected == 'rock' || playerSelected == 'paper' || playerSelected == 'scissors'){
+            if(playGround(playerSelected, computerSelected) == 'It is a tie!'){
+                console.log(`Player Score: ${playerScore}`)
+                console.log(`Computer Score: ${computerScore}`)
+            } else if(playGround(playerSelected, computerSelected) == `You win! ${playerSelected} beats ${computerSelected}!`){
+                playerScore++
+                console.log(`Player Score: ${playerScore}`)
+                console.log(`Computer Score: ${computerScore}`)
+            } else {
+                computerScore++
+                console.log(`Player Score: ${playerScore}`)
+                console.log(`Computer Score: ${computerScore}`)        
+            }
+            console.log(playGround(playerSelected, computerSelected))
+        } else {
+            alert("Error - Please type in 'rock' 'scissors' or 'paper'")
+            i--
+        }
+    }
+    if(playerScore > computerScore){
+        console.log(`Congrats you win!`)
+        console.log(`Final Score: Player: ${playerScore} Computer: ${computerScore}`)
+    } else if(playerScore < computerScore){
+        console.log(`Sorry you lose!`)
+        console.log(`Final Score: Player: ${playerScore} Computer: ${computerScore}`)
+    } else {
+        console.log(`It is a tie!`)
+        console.log(`Final Score: Player: ${playerScore} Computer: ${computerScore}`)
     }
 }
 
-// game init
-function game(){
-    update();
-    render();
-}
-
-// loop
-const framePerSecond = 50;
-setInterval(game,1000/framePerSecond);
+pong();
